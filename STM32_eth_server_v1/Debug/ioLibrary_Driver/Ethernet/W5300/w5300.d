4e420a1bc1b6df661a3287f5049c23c7b9329c91
leo <k>
2020-06-18 09:17:33 -0300

    Modificacion total
    Se modifico todo al abrir el proyecto en el workspaces

--- STM32_eth_server_v1/Debug/ioLibrary_Driver/Ethernet/W5300/w5300.d
+++
@@ -1,8 +0,0 @@
-ioLibrary_Driver/Ethernet/W5300/w5300.o: \
- ../ioLibrary_Driver/Ethernet/W5300/w5300.c \
- /home/t440/Documentos/Git/STM32_eth_server_v1/ioLibrary_Driver/Ethernet/wizchip_conf.h \
- /home/t440/Documentos/Git/STM32_eth_server_v1/ioLibrary_Driver/Ethernet/W5500/w5500.h
-
-/home/t440/Documentos/Git/STM32_eth_server_v1/ioLibrary_Driver/Ethernet/wizchip_conf.h:
-
-/home/t440/Documentos/Git/STM32_eth_server_v1/ioLibrary_Driver/Ethernet/W5500/w5500.h:
